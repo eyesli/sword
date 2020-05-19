ab8fd59a251c0c87271e791eb84eb94891228cdc
lideng <453321481@qq.com>
2020-05-19 15:35:11 +0800

    #

--- sword-admin/src/main/java/com/lideng/sword/admin/mq/Receiver.java
+++ sword-admin/src/main/java/com/lideng/sword/admin/mq/Receiver.java
@@ -1,5 +1,6 @@
 package com.lideng.sword.admin.mq;
 
+import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -7,14 +8,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+@Log4j
 @Component
 @RabbitListener(queues = "message")
 public class Receiver {
-    Logger logger = Logger.getLogger(Receiver.class);
+
 
     @RabbitHandler
     public void process(String Str) {
-        logger.info("接收消息："+Str);
-        logger.info("接收消息时间："+new Date());
+        log.info("接收消息："+Str);
+        log.info("接收消息时间："+new Date());
     }
 }
--- sword-admin/src/main/java/com/lideng/sword/admin/mq/Sender.java
+++ sword-admin/src/main/java/com/lideng/sword/admin/mq/Sender.java
@@ -1,5 +1,6 @@
 package com.lideng.sword.admin.mq;
 
+import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,19 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-
+/**
+ * @author lideng
+ */
+@Log4j
 @Component
 public class Sender {
-    Logger logger = Logger.getLogger(Sender.class);
 
     @Autowired
     private AmqpTemplate amqpTemplate;
 
     public String send(){
         String context = "简单消息发送";
-        logger.info("简单消息发送时间："+new Date());
+        log.info("简单消息发送时间："+new Date());
         amqpTemplate.convertAndSend("message", context);
         return "发送成功";
     }
