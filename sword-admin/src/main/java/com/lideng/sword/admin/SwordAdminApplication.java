2811bab304d79d4496926befea2649518a0394ee
eyesli <453321481@qq.com>
2021-05-16 13:29:05 +0800

    #改造成nacos

--- sword-admin/src/main/java/com/lideng/sword/admin/SwordAdminApplication.java
+++ sword-admin/src/main/java/com/lideng/sword/admin/SwordAdminApplication.java
@@ -3,14 +3,14 @@ package com.lideng.sword.admin;
 import com.lideng.sword.common.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ComponentScan(basePackages = "com.lideng.sword.admin.dao")
-@ComponentScan(basePackages = "com.lideng.sword.admin.config")
-@SpringBootApplication(scanBasePackages={"com.lideng.sword.admin"})
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 /**
  * 项目启动入口
  * @author lideng
